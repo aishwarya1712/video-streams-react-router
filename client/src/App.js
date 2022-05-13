b63410b1d10bf2ef8b1db73d5e8b03eaba99842a
Aishwarya Sriram <98.aishu@gmail.com>
2022-05-13 09:05:31 +0530

    added basic react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const PageOne = ()  => {
+    return (
+        <div>Page1!</div>
+    )
+}
+
+const PageTwo = () => {
+    return(
+        <div>Page2!</div>
+    )
+}
+
+const App = () => {
+    return (
+        <BrowserRouter>
+            <Route path="/" exact component={PageOne}></Route>
+            <Route path="/2" component={PageTwo}></Route>
+        </BrowserRouter>
+    )
 }
 
 export default App;
