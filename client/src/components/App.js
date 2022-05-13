34f316404b1f2779f9fabaead20e1c718b03762e
Aishwarya Sriram <98.aishu@gmail.com>
2022-05-13 11:23:33 +0530

    Created Header component - placed inside BrowserRouter since it contains Link tags

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom"
+import Header from "./Header";
 import StreamCreate from "./streams/StreamCreate";
 import StreamDelete from "./streams/StreamDelete";
 import StreamEdit from "./streams/StreamEdit";
@@ -8,15 +9,19 @@ import StreamShow from "./streams/StreamShow";
 
 const App = () => {
     return (
-        <BrowserRouter>
-            <div>
-                <Route path="/" exact component={StreamList}></Route>
-                <Route path="/streams/new" component={StreamCreate}></Route>
-                <Route path="/streams/edit" component={StreamEdit}></Route>
-                <Route path="/streams/delete" component={StreamDelete}></Route>
-                <Route path="/streams/show" component={StreamShow}></Route>
-            </div>
-        </BrowserRouter>
+        <div class="ui container">
+            {/*Can't place Header outside BrowserRoute because it contains a Link tag */}
+            <BrowserRouter>
+                < Header/>
+                <div>
+                    <Route path="/" exact component={StreamList}></Route>
+                    <Route path="/streams/new" component={StreamCreate}></Route>
+                    <Route path="/streams/edit" component={StreamEdit}></Route>
+                    <Route path="/streams/delete" component={StreamDelete}></Route>
+                    <Route path="/streams/show" component={StreamShow}></Route>
+                </div>
+            </BrowserRouter>
+        </div>
     )
 }
 
